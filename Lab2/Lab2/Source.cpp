796bdd803e24a15a6ae86068876c01b8ff855593
alekwtz <alekwtzz@gmail.com>
2017-03-01 17:39:19 +0200

    Implemented backtracking

--- Lab2/Lab2/Source.cpp
+++ Lab2/Lab2/Source.cpp
@@ -1,6 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+int st[10];
+int n;
+
+
+void tipar(int p)
+{
+	for (int i = 0; i <= p; i++)
+	{
+		cout << st[i];
+	}
+	cout << endl;
+
+}
+
+void back(int p)
+{
+	for (int i = 0; i <= 1; i++)
+	{
+		st[p] = i;
+		if (p == n - 1)
+			tipar(p);
+		else
+			back(p + 1);
+	}
+}
+
 void main()
 {
-	cout << "Hello";
+	cin >> n;
+	back(0);
 }
